97eebe8492f43c4a2394d0d518d335f702b94047
JUDE <judeani60@gmail.com>
2026-04-30 00:19:57 +0100

    feat: finish auth endpoints

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,6 +4,7 @@ const { validateBVN, validateNIN } = require("../services/nibss-services");
 const { sanitizePhoneNumber, hashPassword, comparePassword, generateOTP } = require('../utils/helper');
 const { loadEmailTemplate } = require("../utils/emailTemplate");
 const { sendEmail } = require("../utils/mailer");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
     let session = null;
@@ -93,7 +94,7 @@ exports.register = async (req, res) => {
         const recordDate = new Date(result.dob).toISOString().split("T")[0];
 
         if(inputDate !== recordDate) {
-            return res.status(400).json({error: "DOB provided does not match BVN records"});
+            return res.status(400).json({error: `DOB provided does not match ${fields[tAuthType].toUpperCase()} records`});
         }
 
         //hash password
@@ -118,8 +119,8 @@ exports.register = async (req, res) => {
             phone: phoneNumber,
             dob: fields.tDob,
             authId: result.bvn ? result.bvn : result.nin,
-            authType: result.bvn ? "bvn" : "nin",
-            otpCode,
+            authType: fields.tAuthType,
+            tempOtp: otpCode,
             expiresAt
         }], { session });
 
@@ -132,7 +133,7 @@ exports.register = async (req, res) => {
         //commit session
         await session.commitTransaction();
 
-        return res.status(200).json({
+        return res.status(201).json({
             message: "User Created Successfully",
             response: {
                 id: data[0]._id,
@@ -152,9 +153,207 @@ exports.register = async (req, res) => {
 
         //abort session
         if (session) await session.abortTransaction();
-        return res.status(500).json({Error: `Internal Server Error`});
+        return res.status(500).json({ error: "Internal Server Error" });
     } finally {
         //end session
         if (session) session.endSession();
+    }
+}
+
+exports.resendOtp = async (req, res) => {
+    let session = null;
+    try {
+        const { email } = req.body;
+
+        //ensure email is not null/undefined/empty string
+        if(!email || email.trim() === "") {
+            return res.status(400).json({error: "Email must be provided"});
+        }
+
+        const tEmail = email.trim();
+
+        //check if email exist
+        const existingUser = await User.findOne({email: tEmail});
+
+        if(!existingUser || existingUser.emailVerified) {
+            return res.status(400).json({error: "Email does not exist or is already verified"});
+        }
+
+        //check if the time of expiry is remains more than 8 mins
+        if((existingUser.expiresAt.getTime() - Date.now()) > 8 * 60000) {
+            return res.status(429).json({error: "Too Many Requests"});
+        }
+
+        //generate otp and set expiresAt
+        const otpCode = generateOTP();
+        const expiresAt = new Date(Date.now() + 10 * 60000);
+
+        //start session
+        session = await mongoose.startSession();
+
+        //start transaction
+        session.startTransaction();
+
+        existingUser.tempOtp = otpCode;
+        existingUser.expiresAt = expiresAt;
+
+        //save otp and expiration in transaction
+        await existingUser.save({session});
+
+        //load html template
+        const html = loadEmailTemplate("otpTemplate", {otpCode});
+
+        //send email
+        await sendEmail(tEmail, "ONE TIME OTP", html);
+
+        //commit email
+        await session.commitTransaction();
+
+        return res.status(200).json({
+            message: "OTP sent successfully"
+        });
+    } catch(error) {
+        console.log(error);
+
+        //abort session
+        if (session) await session.abortTransaction();
+        return res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+        if (session) session.endSession();
+    }
+}
+
+exports.verifyEmail = async (req, res) => {
+    let session = null;
+    try {
+        const { email, otpCode } = req.body;
+
+        //ensure email and otpCode are not null/undefined/empty string
+        if(!email || email.trim() === "" || !otpCode || otpCode.trim() === "") {
+            return res.status(400).json({error: "Email and OTP must be provided"});
+        }
+
+        const tEmail = email.trim();
+        const tOtpCode = otpCode.trim();
+
+        //find user with the email and otpCode
+        const existingUser = await User.findOne({email: tEmail});
+
+        if(!existingUser) {
+            return res.status(404).json({error: "User not found"});
+        }
+
+        //check if user is verified
+        if(existingUser.emailVerified) {
+            return res.status(400).json({error: "User is already verified"});
+        }
+
+        //check if otp matches saved otp
+        if(existingUser.tempOtp !== tOtpCode) {
+            return res.status(400).json({error: "Invalid OTP"});
+        }
+
+        //check if otp has expired
+        if(Date.now() > existingUser.expiresAt.getTime()) {
+            return res.status(400).json({error: "OTP has expired"});
+        }
+
+        //start session
+        session = await mongoose.startSession();
+
+        //start transaction
+        session.startTransaction();
+
+        //reset otp and expiry to null and emailVerified to true
+        existingUser.tempOtp = null;
+        existingUser.expiresAt = null;
+        existingUser.emailVerified = true;
+
+        await existingUser.save({session});
+
+        //load email template
+        const html = loadEmailTemplate("welcomeTemplate", {
+            firstName: existingUser.firstName,
+            actionLink: "https://google.com"
+        });
+
+        //send email
+        await sendEmail(tEmail, "WELCOME!!", html);
+
+        //commit transaction
+        await session.commitTransaction();
+
+        return res.status(200).json({
+            message: "Email Verified!!"
+        });
+
+    } catch(error) {
+        console.log(error);
+
+        //abort session
+        if (session) await session.abortTransaction();
+        return res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+        if (session) session.endSession();
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //ensure email and password are not null/undefined/empty string
+        if(!email || email.trim() === "" || !password || password.trim() === "") {
+            return res.status(400).json({error: "Email and Password must be provided"});
+        }
+
+        const tEmail = email.trim();
+        const tPassword = password.trim();
+
+        //find user with the email and otpCode
+        const existingUser = await User.findOne({email: tEmail});
+
+        if(!existingUser) {
+            return res.status(404).json({error: "User not found"});
+        }
+
+        //check if user is verified
+        if(!existingUser.emailVerified) {
+            return res.status(400).json({error: "User is not verified"});
+        }
+
+        //verify password
+        const validPassword = await comparePassword(tPassword, existingUser.password);
+
+        if(!validPassword) {
+            return res.status(400).json({error: "Wrong Password"});
+        }
+
+        //jwt sign
+        const token = jwt.sign({
+            id: existingUser._id,
+            email: existingUser.email,
+            phone: existingUser.phone
+        }, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+        return res.status(200).json({
+            message: "User Logged In Successfully",
+            response: {
+                token,
+                id: existingUser._id,
+                firstName: existingUser.firstName,
+                lastName: existingUser.lastName,
+                email: existingUser.email,
+                phone: existingUser.phone,
+                dob: existingUser.dob,
+                authId: existingUser.authId,
+                authType: existingUser.authType,
+                emailVerified: existingUser.emailVerified
+            }
+        })
+
+    } catch(error) {
+        console.log(error);
+        return res.status(500).json({error: "Internal Server Error"});
     }
 }
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require("../controllers/auth-controller")
+const { register, resendOtp, verifyEmail, login } = require("../controllers/auth-controller")
 
 
 router.post("/register", register);
+router.post("/resend-otp", resendOtp);
+router.post("/verify-email", verifyEmail);
+router.post("/login", login);
 
 module.exports = router;
